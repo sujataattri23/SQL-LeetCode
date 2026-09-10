3b3ed850e778da42f9d70a024cbac9d2d7e5809c
Sujata Attri <sujata07attri@gmail.com>
2026-09-10 07:28:20 -0400

    Create Rising Temperature.sql

---
+++ Rising Temperature.sql
@@ -0,0 +1,7 @@
+-- Problem- Rising Temperature
+-- Link- https://leetcode.com/problems/rising-temperature/?envType=study-plan-v2&envId=top-sql-50
+
+select w1.id
+from weather w1, weather w2
+where datediff(w1.recordDate, w2.recordDate) = 1
+and w1.temperature > w2.temperature
